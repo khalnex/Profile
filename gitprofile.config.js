2f93defbb1695f7febcccd6ed98e68677915d893
Khaleel Motala <132395362+khalnex@users.noreply.github.com>
2023-10-17 18:48:51 -0400

    Update gitprofile.config.js
    changing default theme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -127,7 +127,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'halloween',
+    defaultTheme: 'emerald',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
